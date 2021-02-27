eadc4e8d7b9a4139e858878419c325a4604a880b
ZhengchaoW <chaoswanzc@gmail.com>
2021-02-26 20:13:39 -0500

    Add files via upload

--- persistLap.m
+++ persistLap.m
@@ -1,11 +1,20 @@
-% B1 is a (q+1)-boundary matrix, B2 is a q-boundary matrix,
+% B1 is a (q+1)-boundary matrix, B2 is a q-boundary matrix (or 0 if q=0),
 % Gind is an index set for reduction
 
 function [pL,upL] = persistLap(B1,B2,Gind)
+%     B1 = sparse(B1);
+%     B2 = sparse(B2);
     upLaplacian = B1 * B1';
-    downLaplacian = B2' * B2;
-    upL = SchurComp(upLaplacian,Gind); % up persistent Laplacian    
-    pL = upL + downLaplacian; % persistent Laplacian
-    
-
+    if B2 == 0
+        %up persistent Laplacian
+        upL = SchurComp(upLaplacian,Gind);
+        %persistent Laplacian
+        pL = upL;
+    else
+        downLaplacian = B2' * B2;
+        %up persistent Laplacian
+        upL = SchurComp(upLaplacian,Gind);
+        %persistent Laplacian
+        pL = upL + downLaplacian;
+    end 
     
